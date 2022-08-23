8d506e048106b5c1cb4388e7c17ee2948d538449
connorfurby <cafurby@stu.naperville203.org>
2022-08-23 12:39:01 -0500

    Started BlueJ and chap 1 notes

---
+++ My First BlueJ Project-copy/chapter1Notes.java
@@ -0,0 +1,41 @@
+
+/**
+ * Name: Connor Furby 
+ * Date: 8/23/2022
+ * Title: Chapter 1 Notes
+ * Description: Where we are taking ch1 code notes
+ * Who Helped Me: Mr. Hayes
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class chapter1Notes
+{
+    /**
+     * main method
+     * where we are going to code
+     */
+    public static void main(String[] args)
+    {
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+        //These are the basic data types:
+        int x=9,y,z;
+        double num = 5, num1 = 23.4567, num2;
+        char letter = 'd';
+        boolean choice = false;
+        float anum = 7;
+        long a = 9;
+        short b = 5;
+        
+        //These are objects!
+        String word = new String("Connor Furby");
+        Integer number = new Integer(46);
+        Double number2 = new Double(34.5);
+        
+    }
+
+  
+}
